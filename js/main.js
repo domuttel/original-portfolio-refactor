9071ff81058eababfc685992b824d6268c4196f5
Dominic Muttel <dominic.muttel@gmail.com>
2016-03-01 16:44:12 -0700

    added images / jquery

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 // add scripts
 
 $(document).on('ready', function() {
-  console.log('sanity check!');
+    console.log("yo");
+
+    $("#web").click(function(){
+        $("#graphic-projects, #art-projects").hide();
+        $("#web-projects").show();
+    });
+    $("#graphic").click(function(){
+        $("#web-projects, #art-projects").hide();
+        $("#graphic-projects").show();
+    });
+    $("#art").click(function(){
+        $("#web-projects, #graphic-projects").hide();
+        $("#art-projects").show();
+    });
 });
+
+
