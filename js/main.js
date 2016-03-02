c6693c992cfa58af13818fba2b7466cd73fc75a2
Dominic Muttel <dominic.muttel@gmail.com>
2016-03-02 13:49:01 -0700

    added jquery nav

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
-// add scripts
-
 $(document).on('ready', function() {
-    console.log("yo");
-
-    $("#web").click(function(){
-        $("#graphic-projects, #art-projects").hide();
-        $("#web-projects").show();
-    });
-    $("#graphic").click(function(){
-        $("#web-projects, #art-projects").hide();
-        $("#graphic-projects").show();
-    });
-    $("#art").click(function(){
-        $("#web-projects, #graphic-projects").hide();
-        $("#art-projects").show();
+    console.log("Yo, I work it.");
+//event handler function for project list on click of specialty
+    $( ".specialty" ).click(function() {
+      // `this` is the DOM element that is clicked
+        var index = $( ".specialty" ).index( this );
+        if (index === 0) {
+            $( "#projects" ).text( "span web" );
+        }
+        else if (index === 1){
+            $( "#projects" ).text( "span graphic" );
+        }
+        else {
+            $( "#projects" ).text( "span art" );
+        }
     });
 });
 
