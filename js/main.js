738144d315ac352a05ff84b3119ad2b4d8411eef
Dominic Muttel <dominic.muttel@gmail.com>
2016-03-02 19:19:16 -0700

    cleaned up and added font

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 $(document).on('ready', function(e) {
 
     console.log("Yo, I work it.");
@@ -14,9 +13,8 @@ $(document).on('ready', function(e) {
         }
         else if (index === 1){
             $( "#projects" ).html(
-                '<a href="">' + 'Identity' + '</a>' + ', ' +
-                '<a href="">' + 'Print' + '</a>'
-            );
+                '<span>' + 'Identity' + '</span>' + ', ' +
+                '<span>' + 'Print' + '</span>');
         }
         else {
             $( "#projects" ).html(
@@ -24,50 +22,24 @@ $(document).on('ready', function(e) {
                 '<span>' + 'Television Vernacular' + '</span>' + ', ' +
                 '<span>' + 'Uncertainty Principle' + '</span>' + ', ' +
                 '<span>' + 'Remote Patterns' + '</span>' + ', ' +
-                '<span>' + 'Hands' + '</span>' );
+                '<span>' + 'Hands' + '</span>');
         }
 
     });
     //----------------------------------------------
+
+    // On click show Galleria
     $( document ).on("click", "#nl", function() {
-            var noesis = [
-                {
-                    image: 'images/galleries/noesisLandscape/1.jpg',
-                    title: '<em>Document #1</em>, 2011',
-                    description: 'inkjet print, <br> 36 in. (91.44 cm).',
-                },
-                {
-                    image: 'images/galleries/noesisLandscape/2.jpg',
-                    title: '<em>Document #2</em>, 2011',
-                    description: 'inkjet print, <br> 24 x 24 in. (60.96 x 60.96 cm).',
-                },
-                {
-                    image: 'images/galleries/noesisLandscape/3.jpg',
-                    title: '<em>Document #3</em>, 2011',
-                    description: 'inkjet print, <br> 9 x 12.75 in. (22.86 x 32.385 cm).',
-                },
-                {
-                    image: 'images/galleries/noesisLandscape/4.jpg',
-                    title: '<em>Document #4</em>, 2011',
-                    description: 'inkjet print, <br> 11.75 x 17.75 in. (29.845 x 45.085 cm).',
-                }
-            ];
+        // Load Galleria Theme
+        Galleria.loadTheme('js/galleria/galleria.classic.js');
 
-            Galleria.loadTheme('js/galleria/galleria.classic.js');
-
-            // Initialize Galleria
-            $('#galleria').galleria({
-                imageCrop: false,
-                dataSource: noesis,
-                transition: 'fadeslide',
-                responsive: true
-            });
+        // Initialize Galleria
+        $('#galleria').galleria({
+            imageCrop: false,
+            dataSource: noesis,
+            transition: 'fadeslide',
+            responsive: true
+        });
     });
-
-
+    //----------------------------------------------
 });
-
-
-// Galleria.run('.galleria', {
-//     dataSource: noesis
-// });
