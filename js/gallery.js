1a396f6040dc9d1681328046a459321afb056f15
Dominic Muttel <dominic.muttel@gmail.com>
2016-03-02 22:37:26 -0700

    completed image gallery functions

--- js/gallery.js
+++ js/gallery.js
@@ -1,22 +1,66 @@
-var noesis = [
-    {
-        image: 'images/galleries/noesisLandscape/1.jpg',
-        title: '<em>Document #1</em>, 2011',
-        description: 'inkjet print, <br> 36 in. (91.44 cm).',
-    },
-    {
-        image: 'images/galleries/noesisLandscape/2.jpg',
-        title: '<em>Document #2</em>, 2011',
-        description: 'inkjet print, <br> 24 x 24 in. (60.96 x 60.96 cm).',
-    },
-    {
-        image: 'images/galleries/noesisLandscape/3.jpg',
-        title: '<em>Document #3</em>, 2011',
-        description: 'inkjet print, <br> 9 x 12.75 in. (22.86 x 32.385 cm).',
-    },
-    {
-        image: 'images/galleries/noesisLandscape/4.jpg',
-        title: '<em>Document #4</em>, 2011',
-        description: 'inkjet print, <br> 11.75 x 17.75 in. (29.845 x 45.085 cm).',
-    }
-];
+// ART OBJECTS
+    var noesis = [
+        {
+            image: 'images/galleries/noesisLandscape/1.jpg',
+            title: '<em>Document #1</em>, 2011',
+            description: 'inkjet print, <br> 36 in. (91.44 cm).',
+        },
+        {
+            image: 'images/galleries/noesisLandscape/2.jpg',
+            title: '<em>Document #2</em>, 2011',
+            description: 'inkjet print, <br> 24 x 24 in. (60.96 x 60.96 cm).',
+        },
+        {
+            image: 'images/galleries/noesisLandscape/3.jpg',
+            title: '<em>Document #3</em>, 2011',
+            description: 'inkjet print, <br> 9 x 12.75 in. (22.86 x 32.385 cm).',
+        },
+        {
+            image: 'images/galleries/noesisLandscape/4.jpg',
+            title: '<em>Document #4</em>, 2011',
+            description: 'inkjet print, <br> 11.75 x 17.75 in. (29.845 x 45.085 cm).',
+        },
+        {
+            image: 'images/galleries/noesisLandscape/5.jpg',
+            title: '<em>Document #5</em>, 2011',
+            description: 'inkjet print, <br> 10 x 7.5 in. (25.4 x 19.05 cm).',
+        },
+        {
+            image: 'images/galleries/noesisLandscape/6.jpg',
+            title: '<em>Document #6</em>, 2011',
+            description: 'inkjet print, <br> 12.25 x 12.25 in. x 3 (31.115 x 31.115 cm. x 3)',
+        },
+        {
+            image: 'images/galleries/noesisLandscape/7.jpg',
+            title: '<em>Document #7</em>, 2011',
+            description: 'inkjet print, <br> 6.5 x 17.25 in. (16.51 x 43.815 cm).',
+        },
+        {
+            image: 'images/galleries/noesisLandscape/8.jpg',
+            title: '<em>Document #8</em>, 2011',
+            description: 'inkjet print, <br> 36 x 36 in. (91.44 x 91.44 cm).',
+        },
+        {
+            image: 'images/galleries/noesisLandscape/9.jpg',
+            title: '<em>Document #9</em>, 2011',
+            description: 'inkjet print, <br> 19 x 19 in. (48.26 x 48.26 cm).',
+        },
+        {
+            image: 'images/galleries/noesisLandscape/10.jpg',
+            title: '<em>Document #10</em>, 2011',
+            description: 'inkjet print, <br> 14.25 x 10 in. (36.195 x 25.4 cm).',
+        }
+    ];
+// GRAPHIC OBJECTS
+    var identity = [
+        {
+            image: 'images/galleries/logo/8.jpg',
+            title: '',
+            description: ''
+        },
+        {
+            image: 'images/galleries/logo/9.jpg',
+            title: '',
+            description: ''
+        },
+    ];
--- js/main.js
+++ js/main.js
@@ -1,45 +1,69 @@
 $(document).on('ready', function(e) {
-
+    var index;
     console.log("Yo, I work it.");
-
     //event handler function for project list on click of specialty
     $( ".specialty" ).click(function(e) {
         e.preventDefault();
         // `this` is the DOM element that is clicked
-        var index = $( ".specialty" ).index( this );
+        index = $( ".specialty" ).index( this );
         // 'if statement' to control project view on click
         if (index === 0) {
             $( "#projects" ).text( "span web" );
         }
         else if (index === 1){
             $( "#projects" ).html(
-                '<span>' + 'Identity' + '</span>' + ', ' +
-                '<span>' + 'Print' + '</span>');
+                '<span class="image-gallery">' + 'Identity' + '</span>' + ', ' +
+                '<span class="image-gallery">' + 'Print' + '</span>');
+            // console.log($("#projects:first-child"));
         }
         else {
             $( "#projects" ).html(
-                '<span id="nl">' + 'Noesis/Landscape' + '</span>' + ', ' +
-                '<span>' + 'Television Vernacular' + '</span>' + ', ' +
-                '<span>' + 'Uncertainty Principle' + '</span>' + ', ' +
-                '<span>' + 'Remote Patterns' + '</span>' + ', ' +
-                '<span>' + 'Hands' + '</span>');
+                '<span class="image-gallery">' + 'Noesis/Landscape' + '</span>' + ', ' +
+                '<span class="image-gallery">' + 'Television Vernacular' + '</span>' + ', ' +
+                '<span class="image-gallery">' + 'Uncertainty Principle' + '</span>' + ', ' +
+                '<span class="image-gallery">' + 'Remote Patterns' + '</span>' + ', ' +
+                '<span class="image-gallery">' + 'Hands' + '</span>');
         }
 
     });
     //----------------------------------------------
 
-    // On click show Galleria
-    $( document ).on("click", "#nl", function() {
-        // Load Galleria Theme
-        Galleria.loadTheme('js/galleria/galleria.classic.js');
+    //event handler function for gallery to show when project is selected
+    // function replaceDataSource(){
+        $( document ).on("click", ".image-gallery", function() {
+            var indexOfArtGal = $( ".image-gallery" ).index( this );
+            console.log(indexOfArtGal);
+            console.log(index);
+            if (index === 2) {
+                if(indexOfArtGal === 0) {
+                    loadGalleria(noesis);
+                }
+                else if(indexOfArtGal == 1){
+                    loadGalleria();
+                }
+            }
+            else if (index === 1){
+                if(indexOfArtGal === 0) {
+                    loadGalleria(identity);
+                }
+                else if(indexOfArtGal == 1){
+                    loadGalleria();
+                }
+            }
+        });
+    //----------------------------------------------
 
-        // Initialize Galleria
-        $('#galleria').galleria({
+// Load Galleria Theme
+    Galleria.loadTheme('js/galleria/galleria.classic.js');
+// function to repopulate dataSource onclick of gallery
+    function loadGalleria(dataSource1) {
+            $('#galleria').galleria({
             imageCrop: false,
-            dataSource: noesis,
+            dataSource: dataSource1,
             transition: 'fadeslide',
             responsive: true
         });
-    });
-    //----------------------------------------------
+    }
+
+
 });
