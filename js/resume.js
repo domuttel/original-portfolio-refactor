f377805a4227d70fe6c0a856597281c3d505862e
Dominic Muttel <dominic.muttel@gmail.com>
2016-09-29 14:05:45 -0600

    minor update

---
+++ js/resume.js
@@ -0,0 +1,182 @@
+{
+  "basics": {
+    "name": "Dominic Muttel",
+    "label": "Full stack web development",
+    "phone": "+1.970.420.6721",
+    "website": "http://www.dominicmuttel.com",
+    "summary":
+        "I am a 33 year old Web Developer and Graphic Designer currently based in Denver, Colorado. I live and breathe everything that has to do with Web design & development, Graphic design, Product development,  and test driven development. I feel devoted to the entire creative process which makes me an agile team member.\u0003\u0003\n\nI have 4+ years of experience working as a Graphic Designer and am infatuated with Web Development. I am excited to continue my progress in discovering new techniques within the industry to elevate my expertise. I enjoy collaborating with people that share a growth mindset and a willingness to take on difficult problems. \n\nLet’s work together to find solutions.",
+    "location": {
+      "address": "1001 Logan St.",
+      "postalCode": "80203",
+      "city": "Denver",
+      "countryCode": "",
+      "region": "Colorado"
+    },
+    "profiles": []
+  },
+  "work": [
+    {
+      "company": "DOMU Concept",
+      "position": "Founder",
+      "website": "",
+      "startDate": "2013-04-01",
+      "summary": "Freelance graphic design: Poster, Logo, Identity, Flyer, Presentations",
+      "highlights": [],
+      "endDate": "2015-06-01"
+    },
+    {
+      "company": "School of Critical Engagement",
+      "position": "Assistant Project Manager",
+      "website": "",
+      "startDate": "2010-01-01",
+      "summary": "• Lead a campaign that was presented to 100 organizations dedicated to bettering the community  \n• Conceptualized design oriented projects with diverse teams that help with local and global demands",
+      "highlights": [],
+      "endDate": "2015-06-01"
+    },
+    {
+      "company": "Emmanuel Gallery",
+      "position": "Graphic Designer / Gallery Assistant",
+      "website": "",
+      "startDate": "2009-08-01",
+      "summary": "• Spearheaded both graphic and photographic projects in promotion of various exhibitions and lectures \n• Prepared gallery for exhibits, handled artworks, operated front desk, hosted art and artist receptions",
+      "highlights": [],
+      "endDate": "2012-04-01"
+    },
+    {
+      "company": "LW Research Group",
+      "position": "Interviewer",
+      "website": "",
+      "startDate": "2001-01-01",
+      "summary": "• Presented innovative design technologies to focus groups for qualitative and quantitative research studies   \n• Moderated surveys through fieldwork from key sample markets throughout the United States",
+      "highlights": [],
+      "endDate": "2013-06-01"
+    }
+  ],
+  "education": [
+    {
+      "institution": "Galvanize gSchool",
+      "area": "MEAN Stack",
+      "studyType": "Full stack web development",
+      "startDate": "2015-07-06",
+      "gpa": "",
+      "courses": [],
+      "endDate": "2015-12-18"
+    },
+    {
+      "institution": "Front Range Community College",
+      "area": "Photography",
+      "studyType": "Associate of Arts",
+      "startDate": "2006-01-01",
+      "gpa": "",
+      "courses": [],
+      "endDate": "2008-01-01"
+    },
+    {
+      "institution": "University of Colorado at Denver",
+      "area": "Photography",
+      "studyType": "Attended University of Colorado Denver working towards a Bachelor of Fine Arts",
+      "startDate": "2009-01-01",
+      "gpa": "",
+      "courses": [],
+      "endDate": "2012-01-01"
+    }
+  ],
+  "skills": [
+    {
+      "name": "Branding & Identity",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Flyers",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Graphic Design",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Graphic Arts",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Graphics",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Computer Graphics",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Logo Design",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "JavaScript",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Adobe Creative Suite",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "CSS",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Photography",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "HTML",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Posters",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Creative Solutions",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Illustration",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Fine Art",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Web Design",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Contemporary Art",
+      "level": "",
+      "keywords": []
+    },
+    {
+      "name": "Microsoft Office",
+      "level": "",
+      "keywords": []
+    }
+  ]
+}
